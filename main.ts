3ee137d31b90cb091a9dd33f4a0859ab33e8cc98
BuchiProf <46644031+BuchiProf@users.noreply.github.com>
2026-04-01 08:57:03 +0200

    retire espaces

--- main.ts
+++ main.ts
@@ -1,21 +1,21 @@
 /**
  * Blocs pour le contrôle du module 4-Relais Seeed Studio.
  */
-// % color="#03AA14" icon="\uf013" block="Relais Seeed"
+//%color="#03AA14" icon="\uf013" block="Relais Seeed"
 namespace seeedRelay {
 
     export enum ActionRelais {
-        // % block="allumé"
+        //% block="allumé"
         Active = 1,
-        // % block="éteint"
+        //% block="éteint"
         Desactive = 0
     }
 
     /**
      * Change l'état d'un relais spécifique (1 à 4)
      */
-    // % block="mettre le relais %num à %state"
-    // % num.min=1 num.max=4 num.defl=1
+    //% block="mettre le relais %num à %state"
+    //% num.min=1 num.max=4 num.defl=1
     export function controlRelais(num: number, state: ActionRelais): void {
         let buf = pins.createBuffer(2);
         buf[0] = num;
