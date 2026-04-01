d81dce9751a87f804a19fd0f9a8c995ce24d26c9
BuchiProf <46644031+BuchiProf@users.noreply.github.com>
2026-04-01 08:08:18 +0200

    premier main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,32 @@
+namespace seeedRelay {
 
+    // Cette partie crée un menu déroulant pour ON et OFF
+    export enum RelayState {
+        // % block="allumé"
+        On = 1,
+        // % block="éteint"
+        Off = 0
+    }
+
+    /**
+     * Change l'état d'un relais spécifique (1 à 4)
+     */
+    // % block="mettre le relais %num à %state"
+    // % num.min=1 num.max=4 num.defl=1
+    export function controlRelay(num: number, state: RelayState): void {
+        let buf = pins.createBuffer(2);
+        buf[0] = num;
+        buf[1] = state;
+        pins.i2cWriteBuffer(0x11, buf);
+    }
+
+    /**
+     * Éteint tous les relais
+     */
+    // % block="éteindre tous les relais"
+    export function allOff(): void {
+        for (let i = 1; i <= 4; i++) {
+            controlRelay(i, RelayState.Off);
+        }
+    }
+}
--- test.ts
+++ test.ts
@@ -1,1 +1,2 @@
 // les tests vont ici ; cela ne sera pas compilé si ce paquet est utilisé en tant qu'extension.
+seeedRelay.controlRelay(1, seeedRelay.RelayState.On)
